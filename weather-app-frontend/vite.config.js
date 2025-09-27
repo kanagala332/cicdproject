1bc333796ad98808d88c344f726200b50b387bcd
Kanagala332 <Kanagalaharshavardhan@gmail.com>
2025-09-27 07:33:46 +0530

    first commit

--- weather-app-frontend/vite.config.js
+++ weather-app-frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/weather-frontend/'  // <-- must match your Tomcat context path
 })
+
