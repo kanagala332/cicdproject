4cbe85ac58a0f71500cfd3066f8e9476f1880d54
kanagala332 <Kanagalaharshavardhan@gmail.com>
2025-09-27 08:24:51 +0530

    Update vite.config.js

--- weather-app-frontend/vite.config.js
+++ weather-app-frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-frontend/'  // <-- must match your Tomcat context path
+  base: '/weather-frontend/', // <-- must match Tomcat folder name
 })
 
